2617b6dc83041b0545d3f33e684e1443af804be5
Ugwuanyi Christopher Chikodili <christopherchikodili@gmail.com>
2023-01-25 18:34:26 -0800

    team project on printf

--- _print.c
+++ _print.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-			++;
+			++i;
 			printed = handle_print(format, &i, list, buffer,
 					flags, width, precision, size);
 			if (printed == -1)
